5eabb6783a47c97c09fb772b96fa9b3fa173bfcf
peznuss <peznuss@Mac-mini-de-peznuss.local>
2025-06-05 19:03:44 -0600

    add bastion.tf module

--- main.tf
+++ main.tf
@@ -42,7 +42,7 @@ module "vpc" {
 
 # --- 2. Crear Servidores Bastion ---
 module "bastion" {
-  source = "./modules/bastion"
+  source = "./modules/bastion_server"
 
   project_name     = var.project_name
   vpc_id           = module.vpc.vpc_id
